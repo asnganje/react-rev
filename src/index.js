17428dfcdd359220bfff04b6d5f7e46706aee56f
asnganje <abd.nganje@gmail.com>
2023-03-22 16:02:36 +0300

    added support project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from './App'
-const el = document.getElementById('root')
-const root = ReactDOM.createRoot(el);
-root.render(
-    <div>
-        <App />
-    </div>
-)
